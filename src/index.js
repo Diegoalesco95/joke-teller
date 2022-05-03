4dec2dad3e04107d8ad9b7bfe34f4dcfa382e70e
Diegoalesco95 <42728227+Diegoalesco95@users.noreply.github.com>
2022-05-03 17:08:27 -0500

    :sparkles: Get the joke and tell it

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-console.log('Hello World');
+const apiKey = ""
+const jokesApi = 'https://api.jokes.one'
+const uri = '/jod'
+
+const buttonElement = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+
+function toggleButton() {
+  buttonElement.disabled = !buttonElement.disabled;
+}
+
+
+function tellJoke(textToSpeech = "Hello, I'm a Robot") {
+  VoiceRSS.speech({
+    key: apiKey,
+    src: textToSpeech,
+    hl: 'en-us',
+    v: 'Mike',
+    r: 0,
+    c: 'mp3',
+    f: '44khz_16bit_stereo',
+    ssml: false
+  });
+}
+
+async function getJoke() {
+  try {
+    toggleButton();
+    const response = await fetch(`${jokesApi}${uri}`)
+    const data = await response.json()
+    const textToSpeech = data.contents.jokes[0].joke.text || 'No joke for you, sorry'
+    tellJoke(textToSpeech)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+function init() {
+  buttonElement.addEventListener('click', getJoke);
+  audioElement.addEventListener('ended', toggleButton);
+}
+
+init();
